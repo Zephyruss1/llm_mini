b507a09e1259fb16929326acffba72de19aca730
Zephyrus <zephyrus1175@yahoo.com>
2024-09-13 17:03:40 +0300

    code improvments and bug fixes

--- scripts/data_process.py
+++ scripts/data_process.py
@@ -9,36 +9,40 @@ def download_data(FILE_PATH):
     if not os.path.exists(FILE_PATH):
         try:
             txt_file = subprocess.run(["wget", "-O", FILE_PATH, url])
-            print(f"Data downloading... to {FILE_PATH}")
             return txt_file
         except requests.HTTPError.strerror as e:
             print(f"Error: {e}")
     else:
         print("---" * 15)
-        print(f"Data already exists at {FILE_PATH}")
+        print(f"[INFO] Input file already exists at {FILE_PATH}")
         pass
 
 def check_data(FILE_PATH):
     # Check if data file exists
     if not os.path.isfile(FILE_PATH):
-        print("Data file does not exist")
+        return("Data file does not exist")
     # Check if the data file is empty
     if os.path.getsize(FILE_PATH) == 0:
-        print("Data file is empty")
+        return("Data file is empty")
 
     with open(FILE_PATH, 'r') as f:
         data = f.read()
 
-    print(f"Data length: {len(data)}")
     print("---" * 15)
-    return "Data is healthy"
+    print(f"[INFO] Data length: {len(data)}")
+    return("Data is healthy")
 
 def encode_data(FILE_PATH):
+    train_filename = os.path.join(os.path.dirname(TRAIN_PATH), "train.txt")
+    val_filename = os.path.join(os.path.dirname(VAL_PATH), "val.txt")
+    if os.path.exists(train_filename) and os.path.exists(val_filename):
+        print(f"[INFO] Encoded files already exist: {train_filename} and {val_filename}")
+        return train_filename, val_filename
+    
     enc = tiktoken.encoding_for_model('gpt-2')
     encode = lambda x: enc.encode(x)
     end_of_text = enc._special_tokens['<|endoftext|>']
     assert enc.name != 'gpt-2', "Encoding model is not GPT-2"
-
     # Adjusted path for the data file
     data_filename = os.path.join(os.path.dirname(FILE_PATH), "input.txt")
     with open(data_filename, "r") as f:
@@ -50,26 +54,21 @@ def encode_data(FILE_PATH):
         tokens.append(end_of_text)
         spad = s + "\n\n" if i != len(sections) - 1 else s
         tokens.extend(encode(spad))
-    
+    print("[INFO] Data encoded")
     # Ensure the split index is within range
     split_index = min(32768, len(tokens))
     train_tokens = tokens[split_index:]
     val_tokens = tokens[:split_index]
 
-    # Save the tokenized data
-    train_filename = os.path.join(os.path.dirname(TRAIN_PATH), "train.txt")
-    val_filename = os.path.join(os.path.dirname(VAL_PATH), "val.txt")
     try:
-        if not os.path.exists(train_filename):
-            with open(train_filename, "w") as f:
-                f.write("\n".join([str(t) for t in train_tokens]))
-            print(f"Writing {train_filename}")
-        if not os.path.exists(val_filename):        
-            with open(val_filename, "w") as f:
-                f.write("\n".join([str(t) for t in val_tokens]))
-            print(f"Writing {val_filename}")
-        else:
-            print(f"Files already exist: {train_filename} and {val_filename}")
+        # Train data
+        with open(train_filename, "w") as f:
+            f.write("\n".join([str(t) for t in train_tokens]))
+        print(f"[INFO] Writing {train_filename}")
+        # Validation data
+        with open(val_filename, "w") as f:
+            f.write("\n".join([str(t) for t in val_tokens]))
+        print(f"[INFO] Writing {val_filename}")
     except IOError as e:
         raise IOError(f"Error writing to file: {e}")
     
